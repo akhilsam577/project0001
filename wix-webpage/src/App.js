e37eb4c83d107a2112ce082fb1a854fac7e32670
akhilsam577 <akhilsam577@gmail.com>
2022-01-20 03:22:05 +0530

    sec commit

--- wix-webpage/src/App.js
+++ wix-webpage/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import PageContent from "./PageContent";
 import Blocks from "./Blocks";
+import Block2 from "./Block2";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/" element={<Blocks />} exact />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Block2 />} exact />
         </Routes>
       </Router>
     </div>
---
+++ wix-webpage/src/Block2.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Block2.css";
+
+const Block2 = () => {
+  return (
+    <div class="container">
+      <h1>Professionally Designed Website Templates</h1>
+      <p>
+        Choose from 500+ customizable website templates that are built to meet
+        your business needs.
+      </p>
+
+      <button>See All Website Templates</button>
+    </div>
+  );
+};
+
+export default Block2;
